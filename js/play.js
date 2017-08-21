c43e062a847909d562ddcd80f3ba6e9600d8148d
Łukasz Kister <look4z@gmail.com>
2017-08-21 16:44:25 +0200

    update lava added

--- js/play.js
+++ js/play.js
@@ -9,15 +9,18 @@ var playState = {
     preload: function() {
         game.load.spritesheet('player', 'assets/zombie_anim.png', 195, 273);
         game.load.image("ground", "assets/ground.png");
-        game.load.image("sky", "assets/sky.png");
+        game.load.image("sky", "assets/sky_wide.png");
         game.load.image("meat", "assets/meat.png");
+        game.load.image("lava", "assets/lava.png");
     },
 
     create: function() {
         // fizyka
         game.physics.startSystem(Phaser.Physics.ARCADE);
         // niebo
-        game.add.sprite(0, 0, 'sky');
+        game.add.tileSprite(0, 0, 8000, 600, 'sky');
+        game.world.setBounds(0, 0, 8000, 600);
+
         // zombiak
         player = game.add.sprite(30, 410, 'player');
         player.frame = 5;
@@ -28,10 +31,12 @@ var playState = {
         player.body.collideWorldBounds = true;
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [6, 7, 8, 9], 10, true);
+        game.camera.follow(player);
+
         // ziemia
         platforms = game.add.group();
         platforms.enableBody = true;
-        var ground = platforms.create(0, game.world.height - 54, 'ground');
+        var ground = platforms.create(0, game.world.height - 56, 'ground');
         ground.body.immovable = true;
         ground.scale.setTo(2, 2);
         // platformy
@@ -42,6 +47,14 @@ var playState = {
         ledge = platforms.create(0, 250, 'ground');
         ledge.scale.setTo(0.25, 0.25);
         ledge.body.immovable = true;
+
+        // lava 
+        lavaGround = game.add.group();
+        lavaGround.enableBody = true;
+        var lava = lavaGround.create(1600, game.world.height - 50, "lava");
+        lava.body.immovable = true;
+        lava.scale.setTo(3, 1);
+
 
         // meat 
         meats = game.add.group();
@@ -56,6 +69,7 @@ var playState = {
         cursors = game.input.keyboard.createCursorKeys();
         space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         scoreText = game.add.text(16, 16, 'Meats collected: 0', { fontSize: '32px', fill: '#000' });
+        scoreText.fixedToCamera = true;
     },
 
     update: function() {
@@ -64,16 +78,22 @@ var playState = {
             score += 1;
             scoreText.text = 'Score: ' + score;
         }
+
+        function restart() {
+            game.state.start("play")
+        }
+
         var hitPlatform = game.physics.arcade.collide(player, platforms);
         game.physics.arcade.collide(meats, platforms);
         game.physics.arcade.overlap(player, meats, collectMeat, null, this);
+        game.physics.arcade.overlap(player, lavaGround, restart, null, this)
         player.body.velocity.x = 0;
 
         if (cursors.left.isDown) {
-            player.body.velocity.x = -150;
+            player.body.velocity.x = -250;
             player.animations.play('left');
         } else if (cursors.right.isDown) {
-            player.body.velocity.x = 150;
+            player.body.velocity.x = 250;
             player.animations.play('right');
         } else {
             player.animations.stop();
