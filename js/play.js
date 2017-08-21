7105420a19617c95bb698f2d6647de8dec445164
Łukasz Kister <look4z@gmail.com>
2017-08-21 14:54:30 +0200

    walking animation added

--- js/play.js
+++ js/play.js
@@ -1,32 +1,40 @@
 var cursors;
 var player;
+var scoreText;
+var score = 0;
+var platforms;
 
 var playState = {
 
     preload: function() {
-        game.load.image('player', 'assets/Zombie.png');
+        game.load.spritesheet('player', 'assets/zombie_anim.png', 195, 273);
         game.load.image("ground", "assets/ground.png");
         game.load.image("sky", "assets/sky.png");
         game.load.image("meat", "assets/meat.png");
     },
 
     create: function() {
+
         // fizyka
         game.physics.startSystem(Phaser.Physics.ARCADE);
         // niebo
         game.add.sprite(0, 0, 'sky');
         // ludek do grania
-        player = game.add.sprite(30, game.world.height - 180, 'player');
-        player.scale.setTo(0.25, 0.25);
+        player = game.add.sprite(30, game.world.height - 200, 'player');
+        player.frame = 5;
+        player.scale.setTo(0.5, 0.5);
         game.physics.arcade.enable(player);
         player.body.bounce.y = 0.2;
         player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [6, 7, 8, 9], 10, true);
         // ziemia
         platforms = game.add.group();
         platforms.enableBody = true;
         var ground = platforms.create(0, game.world.height - 54, 'ground');
         ground.body.immovable = true;
+        ground.scale.setTo(2, 2);
         // platformy
         var ledge = platforms.create(400, 350, 'ground');
         ledge.scale.setTo(0.5, 0.25);
@@ -47,12 +55,16 @@ var playState = {
         }
 
         cursors = game.input.keyboard.createCursorKeys();
+        scoreText = game.add.text(16, 16, 'Meats collected: 0', { fontSize: '32px', fill: '#000' });
     },
 
     update: function() {
 
+
         function collectMeat(player, meat) {
             meat.kill();
+            score += 1;
+            scoreText.text = 'Score: ' + score;
         }
         var hitPlatform = game.physics.arcade.collide(player, platforms);
         game.physics.arcade.collide(meats, platforms);
@@ -61,13 +73,21 @@ var playState = {
 
         if (cursors.left.isDown) {
             player.body.velocity.x = -150;
+            player.animations.play('left');
         } else if (cursors.right.isDown) {
             player.body.velocity.x = 150;
+            player.animations.play('right');
+        } else {
+            player.animations.stop();
+            player.frame = 5;
         }
 
 
         if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
             player.body.velocity.y = -350;
+        }
+        if (cursors.up.isDown || !player.body.touching.down) {
+            player.animations.stop();
         }
 
     }
